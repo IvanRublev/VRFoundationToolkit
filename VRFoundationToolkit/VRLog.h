21bf79c73781243326d8a5852bc660cb11723f44
Ivan Rublev <ivan@ivanrublev.me>
2014-05-02 20:17:50 +0400

    Added file and function info to default logging macro.

--- VRFoundationToolkit/VRLog.h
+++ VRFoundationToolkit/VRLog.h
@@ -32,20 +32,21 @@
 #define VRLOG_ALWAYS KSLOG_ALWAYS
 #endif
 
+#define iVRLOG(FMT, ...) NSLog(@"%s:%d %s "FMT, strrchr(__FILE__, '/') == 0 ? __FILE__ : strrchr(__FILE__, '/')+1, __LINE__, __PRETTY_FUNCTION__, ##__VA_ARGS__)
 #ifndef VRLOG_TRACE
-#define VRLOG_TRACE NSLog
+#define VRLOG_TRACE iVRLOG
 #endif
 #ifndef VRLOG_DEBUG
-#define VRLOG_DEBUG NSLog
+#define VRLOG_DEBUG iVRLOG
 #endif
 #ifndef VRLOG_ERROR
-#define VRLOG_ERROR NSLog
+#define VRLOG_ERROR iVRLOG
 #endif
 #ifndef VRLOG_INFO
-#define VRLOG_INFO NSLog
+#define VRLOG_INFO iVRLOG
 #endif
 #ifndef VRLOG_ALWAYS
-#define VRLOG_ALWAYS NSLog
+#define VRLOG_ALWAYS iVRLOG
 #endif
 
 // Macros for log error then assert with same message then return.
